5b3e5d0cb5df32cdc4a5f835f87801d5afd4adad
aRakkolainen <aino.rakkolainen@gmail.com>
2023-11-17 14:47:21 +0200

    Task 2

--- routes/index.js
+++ routes/index.js
@@ -5,14 +5,15 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Week4' });
 });
-
-router.get('/recipe/:food', function(req, res) {
+//Task 1
+router.get('/recipe/:food', function(req, res, next) {
   //let recipeName = req.params.food; 
   let recipe = req.body;
   //res.render('recipe', {title: req.body.name, ingredients: recipe.ingredients, instructions: recipe.instructions});
   res.send(recipe);
+}, (req, res, next) => {
+  res.render('index', {title: req.body.name, ingredients: recipe.ingredients, instructions: recipe.instructions})
 })
-
 router.post("/recipe/", function(req, res) {
 
 })
